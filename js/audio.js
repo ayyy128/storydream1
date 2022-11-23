29c7b60499bae0ef6fdeee0d6967fd7b0f8081d5
ayyy128 <116692189+ayyy128@users.noreply.github.com>
2022-11-23 09:55:06 +0900

    s
    s

--- js/audio.js
+++ js/audio.js
@@ -51,8 +51,8 @@ $(document).ready(function () { //문서준비이벤트
     const best_all = new Swiper('.best_all', {
 
         centeredSlides: false,  // 첫번째 슬라이드 가운데 배치
-        slidesPerView: '3.6',  // 한 슬라이드에 보여줄 갯수
-        spaceBetween: 20,  // 슬라이드 사이 여백
+        slidesPerView: '3.8',  // 한 슬라이드에 보여줄 갯수
+        spaceBetween: -10,  // 슬라이드 사이 여백
 
         loop: true,  // 슬라이드 반복 여부
         speed: 1000  // 슬라이드 동작 속도 기본이 300인데 부드럽게 넘기기 위해 1000으로 사용
@@ -64,14 +64,22 @@ $(document).ready(function () { //문서준비이벤트
     const tag_list1 = new Swiper('.tag_list1', {
 
         centeredSlides: false,  // 첫번째 슬라이드 가운데 배치
-        slidesPerView: '3',  // 한 슬라이드에 보여줄 갯수
-        spaceBetween: 30,  // 슬라이드 사이 여백
+        slidesPerView: '3.2',  // 한 슬라이드에 보여줄 갯수
+        spaceBetween: 20,  // 슬라이드 사이 여백
 
         navigation: {
             nextEl: '.swiper-button-next',  // 다음 버튼
             prevEl: '.swiper-button-prev',  // 이전 버튼
         },
 
+        breakpoints: {
+
+            768: {
+                slidesPerView: '3',  // 한 슬라이드에 보여줄 갯수
+                spaceBetween: 30,  // 슬라이드 사이 여백
+            },
+
+        },
 
         loop: true,  // 슬라이드 반복 여부
         speed: 1000  // 슬라이드 동작 속도 기본이 300인데 부드럽게 넘기기 위해 1000으로 사용
@@ -120,7 +128,7 @@ $(document).ready(function () { //문서준비이벤트
     const up_today = new Swiper('.up_today', {
 
         centeredSlides: false,  // 첫번째 슬라이드 가운데 배치
-        slidesPerView: '3.5',  // 한 슬라이드에 보여줄 갯수
+        slidesPerView: '3.8',  // 한 슬라이드에 보여줄 갯수
         spaceBetween: 20,  // 슬라이드 사이 여백
 
         loop: true,  // 슬라이드 반복 여부
--- js/search.js
+++ js/search.js
@@ -14,8 +14,17 @@ $(document).ready(function () { //문서준비이벤트
     const best_all = new Swiper('.best_all', {
 
         centeredSlides: false,  // 첫번째 슬라이드 가운데 배치
-        slidesPerView: '3.6',  // 한 슬라이드에 보여줄 갯수
+        slidesPerView: '4',  // 한 슬라이드에 보여줄 갯수
         spaceBetween: 20,  // 슬라이드 사이 여백
+
+        breakpoints: {
+
+            390: {
+                slidesPerView: '3.8',  // 한 슬라이드에 보여줄 갯수
+                spaceBetween: 20,  // 슬라이드 사이 여백
+            },
+
+        },
 
         loop: true,  // 슬라이드 반복 여부
         speed: 1000  // 슬라이드 동작 속도 기본이 300인데 부드럽게 넘기기 위해 1000으로 사용
